05d2d802f05f44861a2f535629adac2f936d4658
Michael Arakilian <arakilian0@gmail.com>
2019-09-23 12:51:59 -0700

    FIXED hero text section

--- src/assets/scripts/views/index.js
+++ src/assets/scripts/views/index.js
@@ -2,7 +2,7 @@ let navBars = document.querySelector(".nav-bars");
 let mobileHeader = document.querySelector(".mobile-header");
 let mobileHeaderBody = document.querySelector(".mobile-header-body");
 let navCloser = document.querySelector(".nav-closer");
-let heroTitle = document.querySelector("#hero-title");
+let heroTitle = document.querySelector(".hero-text-middle");
 
 navBars.addEventListener("click", function() {
 	mobileHeader.classList.remove('mobile-header-close-animation');
@@ -17,7 +17,7 @@ navCloser.addEventListener("click", function() {
 		mobileHeader.style.display = "none";
 	},600);
 	mobileHeader.classList.add('mobile-header-close-animation');
-	heroTitle.style.display = "block";
+	heroTitle.style.display = "flex";
 	navCloser.style.display = "none";
 	mobileHeaderBody.style.display = "none";
 });
