6756430b36a3664c1b826c8c2eefe6d5a032af05
Michael Arakilian <arakilian0@gmail.com>
2019-09-27 14:07:16 -0700

    MAJOR updates to resume section (template stage)

--- src/assets/scripts/views/index.js
+++ src/assets/scripts/views/index.js
@@ -12,6 +12,10 @@ let bodyTag = document.querySelector('body');
 let resumeCloser = document.querySelector('.resume-closer');
 let flippedFooter = document.querySelector('.flipped-footer');
 let footerTag = document.querySelector('footer');
+let stars1 = document.querySelector('#stars');
+let stars2 = document.querySelector('#stars2');
+let stars3 = document.querySelector('#stars3');
+
 
 navBars.addEventListener("click", function() {
 	mobileHeader.classList.remove('mobile-header-close-animation');
@@ -36,9 +40,15 @@ resumeBtn.addEventListener("click", function() {
 	flipBoxContainer.style.transform = "rotateY(-180deg)";
 	flipBoxContainer.style.transition = "transform 1.6s";
 	flippedFooter.style.display = "block";
+	flipBoxBackContent.style.animation = "animFlipBackBoxContentOP 0.8s";
+	flipBoxFront.style.animation = "animFlipBackBoxContent 0.8s";
 	setTimeout(function() {
 		flipBoxBackContent.style.display = "block";
+		flippedFooter.style.display = "none";
 		flipBoxFront.style.display = "none";
+		stars1.style.width = "0";stars1.style.height = "0";
+		stars2.style.width = "0";stars2.style.height = "0";
+		stars3.style.width = "0";stars3.style.height = "0";
 	}, 600);
 });
 
@@ -47,8 +57,15 @@ resumeCloser.addEventListener("click", function() {
 	flipBoxContainer.style.transition = "transform 1.6s";
 	// flipBoxBackContent.style.display = "none";
 	// footerTag.style.display = "none";
+	// flipBoxBackContent.style.animation = "animFlipBackBoxContent 0.6s";
+	flipBoxBackContent.style.animation = "animFlipBackBoxContent 0.6s";
+	flipBoxFront.style.animation = "animFlipBackBoxContentOP 0.6s";
 	setTimeout(function() {
 		flipBoxFront.style.display = "block";
 		flipBoxBackContent.style.display = "none";
+		flippedFooter.style.display = "block";
+		stars1.style.width = "1px";stars1.style.height = "1px";
+		stars2.style.width = "2px";stars2.style.height = "2px";
+		stars3.style.width = "2.5px";stars3.style.height = "2.5px";
 	}, 600);
 });
